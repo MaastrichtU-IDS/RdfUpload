364a06e7ddad28ff2b478bf07f83eb6f10b5e490
vemonet <vincent.emonet@gmail.com>
2018-08-17 00:12:34 +0200

    now iterate over nq, nt, n3, rdf, ttl files if directory if provided

--- src/main/java/nl/unimaas/ids/loader/HttpLoader.java
+++ src/main/java/nl/unimaas/ids/loader/HttpLoader.java
@@ -1,10 +1,15 @@
 package nl.unimaas.ids.loader;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.RegexFileFilter;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.http.HTTPRepository;
 import org.eclipse.rdf4j.rio.Rio;
 
 import java.io.File;
+import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * A class to upload to GraphDB HTTP endpoint
@@ -28,7 +33,22 @@ public class HttpLoader {
 			if(!inputFile.canRead())
 				throw new SecurityException("Can not read from input file \"" + inputFile.getAbsolutePath() + "\"");
 
-			conn.add(new File(filePath), null, Rio.getParserFormatForFileName(inputFile.getName()).get());
+			if (inputFile.isDirectory()) {
+				Collection files = FileUtils.listFiles(
+						inputFile,
+						new RegexFileFilter(".*\\.(nt|nq|n3|ttl|rdf)"),
+						DirectoryFileFilter.DIRECTORY
+				);
+				// Recursively iterate over files in the directory
+				Iterator<File> iterator = files.iterator();
+				while (iterator.hasNext()) {
+					File f = iterator.next();
+					System.out.println("Converting: " + f);
+					conn.add(f, null, Rio.getParserFormatForFileName(f.getName()).get());
+				}
+			} else {
+				conn.add(new File(filePath), null, Rio.getParserFormatForFileName(inputFile.getName()).get());
+			}
 		} catch (Exception e) {
 			throw e;
 		}
