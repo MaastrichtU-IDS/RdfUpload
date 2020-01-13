0be7e73afa071e8f7332fb8f1400afa734cf5e02
Vincent Emonet <vincent.emonet@gmail.com>
2020-01-13 17:52:38 +0100

    fix directory load by commenting DirectoryScanner

--- src/main/java/nl/unimaas/ids/RdfUpload.java
+++ src/main/java/nl/unimaas/ids/RdfUpload.java
@@ -1,8 +1,13 @@
 package nl.unimaas.ids;
 
 import java.io.File;
+import java.util.Collection;
+import java.util.Iterator;
 
 import org.eclipse.rdf4j.repository.Repository;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.RegexFileFilter;
 import org.apache.tools.ant.DirectoryScanner;
 
 import picocli.CommandLine;
@@ -17,15 +22,42 @@ public class RdfUpload {
 				printUsageAndExit();
 
 			Repository repo = SparqlRepositoryFactory.getRepository(cli.dbUrl, cli.repositoryId, cli.username, cli.password);
+//			File inputFile = new File(cli.inputFile);
 			
-			DirectoryScanner scanner = new DirectoryScanner();
-			scanner.setIncludes(new String[]{cli.inputFile.substring(cli.inputFile.startsWith("/") ? 1 : 0)});
-			scanner.setBasedir(new File("/"));
-			scanner.setCaseSensitive(false);
-			scanner.scan();
-			for(String inputFilePath : scanner.getIncludedFiles()) {
-				SparqlLoader.uploadRdf("/" + inputFilePath, repo, cli.graphUri);
-			}
+			SparqlLoader.uploadRdf(cli.inputFile, repo, cli.graphUri);
+			
+//			if (inputFile.isDirectory()) {
+//				System.out.println("before file filter ");
+//				// If the provided input path is a directory, then we iterate over files if this dir
+//				Collection<File> files = FileUtils.listFiles(
+//						inputFile,
+//						new RegexFileFilter(".*\\.(nt|nq|n3|ttl|rdf)(\\.gz)*"),
+//						DirectoryFileFilter.DIRECTORY
+//				);
+//				System.out.println("before iterator directory: ");
+//				System.out.println(files.size());
+//				System.out.println(files.toString());
+//				// Recursively iterate over files in the directory
+//				Iterator<File> iterator = files.iterator();
+//				while (iterator.hasNext()) {
+//					File f = iterator.next();
+//					System.out.println("Uploading: " + f);
+//				}
+//			}
+			
+//			DirectoryScanner scanner = new DirectoryScanner();
+//			scanner.setIncludes(new String[]{cli.inputFile.substring(cli.inputFile.startsWith("/") ? 1 : 0)});
+//			.(nt|nq|n3|ttl|rdf)
+//			String[] includes = {"**\\*.nt"};
+//			scanner.setIncludes(includes);
+//			scanner.setBasedir(new File("/data"));
+//			scanner.setCaseSensitive(false);
+//			scanner.scan();
+//			for(String inputFilePath : scanner.getIncludedFiles()) {
+//				System.out.println("inputFilePath in scanner");
+//				System.out.println(inputFilePath);
+//				SparqlLoader.uploadRdf("/" + inputFilePath, repo, cli.graphUri);
+//			}
 
 		} catch (Exception e) {
 			printUsageAndExit(e);
